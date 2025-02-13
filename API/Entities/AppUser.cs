288b5d93ef7384d1c7d392eb1c36f6b87d068a08
boxalarm <16870969+boxalarm@users.noreply.github.com>
2025-02-13 18:58:04 -0500

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 namespace API.Entities;
 
@@ -6,4 +7,6 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
